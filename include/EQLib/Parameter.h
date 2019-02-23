bab503f480357a1fa403da5ebbe98a6a108ce2ba
Thomas Oberbichler <thomas.oberbichler@gmail.com>
2019-02-23 22:22:11 +0100

    Update parameter

--- include/EQLib/Parameter.h
+++ include/EQLib/Parameter.h
@@ -10,53 +10,37 @@ private:    // member variables
     double m_act_value;
     double m_target;
     double m_result;
+    bool m_isfixed;
 
 public:     // constructors
-    Parameter()
-    : m_ref_value(0.0)
-    , m_act_value(0.0)
-    , m_target(0.0)
-    , m_result(0.0)
-    { }
-    
-    Parameter(
-        const Parameter& other)
-    : m_ref_value(other.m_ref_value)
-    , m_act_value(other.m_act_value)
-    , m_target(other.m_target)
-    , m_result(other.m_result)
-    { }
-    
-    Parameter(
-        const Parameter&& other)
-    : m_ref_value(std::move(other.m_ref_value))
-    , m_act_value(std::move(other.m_act_value))
-    , m_target(std::move(other.m_target))
-    , m_result(std::move(other.m_result))
-    { }
-
     Parameter(
         double ref_value,
         double act_value,
         double target,
-        double result)
+        double result,
+        bool isfixed)
     : m_ref_value(ref_value)
     , m_act_value(act_value)
     , m_target(target)
     , m_result(result)
+    , m_isfixed(isfixed)
+    { }
+
+    Parameter()
+    : Parameter(0, 0, 0, 0, false)
     { }
 
     Parameter(
         double value,
         double target)
-    : Parameter(value, value, target, 0.0)
+    : Parameter(value, value, target, 0.0, false)
     { }
 
 public:     // getters and setters
     double ref_value() const {
         return m_ref_value;
     }
-    
+
     void set_ref_value(double value) {
         m_ref_value = value;
     }
@@ -64,7 +48,7 @@ public:     // getters and setters
     double act_value() const {
         return m_act_value;
     }
-    
+
     void set_act_value(double value) {
         m_act_value = value;
     }
@@ -72,7 +56,7 @@ public:     // getters and setters
     double target() const {
         return m_target;
     }
-    
+
     void set_target(double value) {
         m_target = value;
     }
@@ -80,14 +64,22 @@ public:     // getters and setters
     double result() const {
         return m_result;
     }
-    
+
     void set_result(double value) {
         m_result = value;
     }
 
+    bool isfixed() const {
+        return m_isfixed;
+    }
+
+    void set_isfixed(bool value) {
+        m_isfixed = value;
+    }
+
 public:     // methods
     Dof dof() {
-        return Dof(&m_ref_value, &m_act_value, &m_target, &m_result);
+        return Dof(&m_ref_value, &m_act_value, &m_target, &m_result, m_isfixed);
     }
 };
 
