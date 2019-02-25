81c6958fb319fa20d703e4fffe2a9569271a634e
Thomas Oberbichler <thomas.oberbichler@gmail.com>
2019-02-25 09:29:57 +0100

    Format

--- include/EQlib/PyElement.h
+++ include/EQlib/PyElement.h
@@ -4,18 +4,19 @@
 
 namespace EQlib {
 
-class PyElement
-: public Element
+class PyElement : public Element
 {
-public:
+public:     // constructor
     using Element::Element;
 
+public:     // getters and setters
     std::vector<Dof> dofs() const override
     {
         using ReturnType = std::vector<Dof>;
         PYBIND11_OVERLOAD_PURE(ReturnType, Element, dofs);
     }
 
+public:     // methods
     std::pair<Matrix, Vector> compute(py::dict options) const override
     {
         using ReturnType = std::pair<Matrix, Vector>;
