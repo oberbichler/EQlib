3156b44e51ed293384a936d4d6db3729fa024817
Thomas Oberbichler <thomas.oberbichler@gmail.com>
2019-05-10 16:19:10 +0200

    Increment version number

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext
 from distutils.version import LooseVersion
 
-EQlib_VERSION = '0.15.0'
+EQlib_VERSION = '0.15.1'
 
 class CMakeExtension(Extension):
     def __init__(self, name, sourcedir=''):
