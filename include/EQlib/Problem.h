1b5c53e69dcbe9699113d4953f39a6b817407b75
Thomas Oberbichler <thomas.oberbichler@gmail.com>
2019-10-30 15:03:46 +0100

    Expose hl_add_diagonal

--- include/EQlib/Problem.h
+++ include/EQlib/Problem.h
@@ -1017,6 +1017,7 @@ public:     // methods: python
             // methods
             // // .def("clone", &Type::clone)
             .def("compute", &Type::compute, "order"_a=2, py::call_guard<py::gil_scoped_release>())
+            .def("hl_add_diagonal", &Type::hl_add_diagonal, "values"_a)
             .def("hl_inv_v", &Type::hl_inv_v)
             .def("hl_v", &Type::hl_v)
         ;
