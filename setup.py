829d4dd5a4b297e1a67cb62297d87a1decb29e9b
Thomas Oberbichler <thomas.oberbichler@gmail.com>
2019-11-06 22:17:44 +0100

    Typo

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ class CMakeBuild(build_ext):
         extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
         cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
                       '-DPYTHON_EXECUTABLE=' + sys.executable,
-                      '-DEQlib_VERSION=' + EQLIB_VERSION]
+                      '-DEQLIB_VERSION=' + EQLIB_VERSION]
 
         cfg = 'Debug' if self.debug else 'Release'
         build_args = ['--config', cfg]
