02c7b75f4369644467ede6e231aeafd6f5cfc52a
thomas <thomas.oberbichler@gmail.com>
2019-11-06 22:51:19 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext
 from distutils.version import LooseVersion
 
-EQLIB_VERSION = '0.21.1'
+EQLIB_VERSION = '0.21.2'
 
 class CMakeExtension(Extension):
     def __init__(self, name, sourcedir=''):
