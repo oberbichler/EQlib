b133ff6a6ebb478eec42429699e91671d36834b7
Thomas Oberbichler <thomas.oberbichler@gmail.com>
2019-11-06 22:18:12 +0100

    Update setup

--- setup.py
+++ setup.py
@@ -67,8 +67,6 @@ setup(
     author_email='thomas.oberbichler@gmail.com',
     ext_modules=[CMakeExtension('eqlib')],
     cmdclass=dict(build_ext=CMakeBuild),
-    install_requires=['cmake', 'numpy'],
-    test_suite='nose.collector',
-    tests_require=['nose'],
+    install_requires=['mkl', 'numpy', 'scipy'],
     zip_safe=False,
 )
