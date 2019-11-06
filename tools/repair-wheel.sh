53713a141416346d38ca04db7134ad65c62bd99f
thomas <thomas.oberbichler@gmail.com>
2019-11-06 23:06:28 +0100

    Update repair-wheel.sh

--- tools/repair-wheel.sh
+++ tools/repair-wheel.sh
@@ -1,6 +1,6 @@
 set -e
 yum -y remove cmake
-/opt/python/${PYTHON_VERSION}/bin/pip install cmake
+/opt/python/${PYTHON_VERSION}/bin/pip install -r requirements.txt
 export PATH=$PATH:/opt/python/${PYTHON_VERSION}/bin/
 /opt/python/${PYTHON_VERSION}/bin/pip wheel .
 auditwheel repair eqlib-*.whl
