634d59c6379c0b8cf10df0279c3a3a8dbe360e14
Thomas Oberbichler <thomas.oberbichler@gmail.com>
2019-11-26 17:28:02 +0100

    Format

--- include/eqlib/Variable.h
+++ include/eqlib/Variable.h
@@ -154,10 +154,8 @@ public:     // python
         using Holder = Pointer<Type>;
 
         py::class_<Type, Holder>(m, "Variable")
-            .def(py::init<double, double, double, bool, double, std::string>(),
-                "value"_a=0.0, "lower_bound"_a=-infinity,
-                "upper_bound"_a=infinity, "is_active"_a=true,
-                "multiplier"_a=1.0, "name"_a="")
+            .def(py::init<double, double, double, bool, double, std::string>(), "value"_a=0.0, "lower_bound"_a=-infinity,
+                "upper_bound"_a=infinity, "is_active"_a=true, "multiplier"_a=1.0, "name"_a="")
             .def(py::init<>())
             .def_property("value", &Type::value, &Type::set_value)
             .def_property("lower_bound", &Type::lower_bound,
