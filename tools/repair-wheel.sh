253b95cf1b3d25de3ae708222b043db2b5140fc8
thomas <thomas.oberbichler@gmail.com>
2019-12-14 10:59:26 +0100

    Delete repair-wheel.sh

--- tools/repair-wheel.sh
+++
@@ -1,6 +0,0 @@
-set -e
-yum -y remove cmake
-/opt/python/${PYTHON_VERSION}/bin/pip install -r requirements.txt
-export PATH=$PATH:/opt/python/${PYTHON_VERSION}/bin/
-/opt/python/${PYTHON_VERSION}/bin/pip wheel .
-auditwheel repair eqlib-*.whl
