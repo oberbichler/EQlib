c4255fd252f9671908e127737b9545887729ad95
Thomas Oberbichler <thomas.oberbichler@gmail.com>
2019-12-28 21:05:15 +0100

    Simplify linear solver interface

--- include/eqlib/LinearSolver.h
+++ include/eqlib/LinearSolver.h
@@ -10,15 +10,32 @@ class LinearSolver {
 private: // types
     using Type = LinearSolver;
 
+private: // variables
+    std::string m_solver_name;
+
 public: // constructors
     virtual ~LinearSolver() = default;
 
 public: // methods
-    virtual std::string solver_name() const = 0;
+    std::string solver_name() const
+    {
+        return m_solver_name;
+    }
 
-    virtual bool analyze(const std::vector<int>& ia, const std::vector<int>& ja, Ref<const Vector> a) = 0;
+    void set_solver_name(const std::string& value)
+    {
+        m_solver_name = value;
+    }
 
-    virtual bool factorize(const std::vector<int>& ia, const std::vector<int>& ja, Ref<const Vector> a) = 0;
+    virtual bool analyze(const std::vector<int>& ia, const std::vector<int>& ja, Ref<const Vector> a)
+    {
+        return false;
+    }
+
+    virtual bool factorize(const std::vector<int>& ia, const std::vector<int>& ja, Ref<const Vector> a)
+    {
+        return false;
+    }
 
     virtual bool solve(const std::vector<int>& ia, const std::vector<int>& ja, Ref<const Vector> a, Ref<const Vector> b, Ref<Vector> x) = 0;
     
@@ -29,25 +46,19 @@ public: // python
         using T::T;
 
     public: // methods
-        virtual std::string solver_name() const
+        virtual bool analyze(const std::vector<int>& ia, const std::vector<int>& ja, Ref<const Vector> a) override
         {
             pybind11::gil_scoped_acquire acquire;
-            PYBIND11_OVERLOAD_PURE(std::string, T, solver_name);
-        }
-    
-        virtual bool analyze(const std::vector<int>& ia, const std::vector<int>& ja, Ref<const Vector> a)
-        {
-            pybind11::gil_scoped_acquire acquire;
-            PYBIND11_OVERLOAD_PURE(bool, T, analyze, ia, ja, a);
+            PYBIND11_OVERLOAD(bool, T, analyze, ia, ja, a);
         }
 
-        virtual bool factorize(const std::vector<int>& ia, const std::vector<int>& ja, Ref<const Vector> a)
+        virtual bool factorize(const std::vector<int>& ia, const std::vector<int>& ja, Ref<const Vector> a) override
         {
             pybind11::gil_scoped_acquire acquire;
-            PYBIND11_OVERLOAD_PURE(bool, T, factorize, ia, ja, a);
+            PYBIND11_OVERLOAD(bool, T, factorize, ia, ja, a);
         }
 
-        virtual bool solve(const std::vector<int>& ia, const std::vector<int>& ja, Ref<const Vector> a, Ref<const Vector> b, Ref<Vector> x)
+        virtual bool solve(const std::vector<int>& ia, const std::vector<int>& ja, Ref<const Vector> a, Ref<const Vector> b, Ref<Vector> x) override
         {
             pybind11::gil_scoped_acquire acquire;
             PYBIND11_OVERLOAD_PURE(bool, T, factorize, ia, ja, a, b, x);
@@ -65,8 +76,9 @@ public: // python
 
         py::class_<Type, Trampoline, Holder>(m, "LinearSolver")
             .def(py::init<>())
+            // read-only properties
+            .def_property("solver_name", &Type::solver_name, &Type::set_solver_name)
             // methods
-            .def("solver_name", &Type::solver_name)
             .def("analyze", &Type::analyze, "ia"_a, "ja"_a, "a"_a)
             .def("factorize", &Type::factorize, "ia"_a, "ja"_a, "a"_a)
             .def("solve", &Type::solve, "ia"_a, "ja"_a, "a"_a, "b"_a, "x"_a);
