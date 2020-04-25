ce92d69f49c30b97a0f31720fe4b1f98748b7446
Thomas Oberbichler <thomas.oberbichler@gmail.com>
2020-04-25 09:01:21 +0200

    Simplify test

--- tests/test_problem.py
+++ tests/test_problem.py
@@ -10,17 +10,6 @@ if __name__ == '__main__':
     pytest.main(sys.argv)
 
 
-class ZeroObjective(eq.Objective):
-    def __init__(self, variables):
-        eq.Objective.__init__(self)
-        self.variables = variables
-
-    def compute(self, g, h):
-        g.fill(0)
-        h.fill(0)
-        return 0
-
-
 class ConstantObjective(eq.Objective):
     def __init__(self, variables, f, g, h):
         eq.Objective.__init__(self)
@@ -36,22 +25,6 @@ class ConstantObjective(eq.Objective):
 
 
 @pytest.fixture
-def zero_problem():
-    x1 = eq.Variable(name='x1', value=2.0)
-    x2 = eq.Variable(name='x2', value=7.0)
-    x3 = eq.Variable(name='x3', value=9.0)
-
-    elements = [
-        ZeroObjective([x1, x2]),
-        ZeroObjective([x2, x3]),
-    ]
-
-    problem = eq.Problem(elements)
-
-    return (elements, problem)
-
-
-@pytest.fixture
 def problem():
     x1 = eq.Variable(name='x1', value=2.0)
     x2 = eq.Variable(name='x2', value=7.0)
@@ -64,12 +37,23 @@ def problem():
 
     problem = eq.Problem(elements)
 
-    return (elements, problem)
+    return problem
+
+
+def test_init(problem):
+    assert_equal(problem.nb_variables, 3)
+    assert_equal(problem.nb_equations, 0)
+
+    assert_equal(problem.structure_hm.rows, 3)
+    assert_equal(problem.structure_hm.cols, 3)
+    assert_equal(problem.structure_hm.ia, [0, 2, 4, 5])
+    assert_equal(problem.structure_hm.ja, [0, 1, 1, 2, 2])
+    assert_equal(problem.structure_hm.nb_nonzeros, 5)
+
+    assert_equal(len(problem.hm_values), 5)
 
 
 def test_compute(problem):
-    elements, problem = problem
-
     problem.compute()
 
     assert_equal(problem.f, 4)
@@ -77,25 +61,17 @@ def test_compute(problem):
     assert_equal(problem.hm.toarray(), [[4, -5.6, 0], [0, 4.2, 6], [0, 0, 11.3]])
 
 
-def test_hm_add_diagonal(zero_problem):
-    elements, problem = zero_problem
-
+def test_hm_add_diagonal(problem):
     problem.compute()
-
-    assert_equal(problem.f, 0)
-    assert_equal(problem.df, np.zeros(3))
-    assert_equal(problem.hm.toarray(), np.zeros((3, 3)))
 
     problem.hm_add_diagonal(1)
 
-    assert_equal(problem.f, 0)
-    assert_equal(problem.df, np.zeros(3))
-    assert_equal(problem.hm.toarray(), np.eye(3))
+    assert_equal(problem.f, 4)
+    assert_equal(problem.df, [2, 8.1, 7])
+    assert_equal(problem.hm.toarray(), [[5, -5.6, 0], [0, 5.2, 6], [0, 0, 12.3]])
 
 
 def test_hm_norm_inf(problem):
-    elements, problem = problem
-
     problem.compute()
 
     norm = problem.hm_norm_inf
@@ -104,8 +80,6 @@ def test_hm_norm_inf(problem):
 
 
 def test_scale(problem):
-    elements, problem = problem
-
     problem.compute()
 
     problem.scale(1.5)
