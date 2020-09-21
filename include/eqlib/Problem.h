e4c7a7ea2980609dd01691060c7e1608b64b650e
Thomas Oberbichler <thomas.oberbichler@gmail.com>
2020-09-21 10:28:32 +0200

    Use get_first_index

--- include/eqlib/Problem.h
+++ include/eqlib/Problem.h
@@ -78,7 +78,6 @@ private: // variables
     std::vector<std::vector<Index>> m_element_g_equation_indices;
     std::vector<std::vector<Index>> m_element_g_variable_indices;
 
-    std::vector<std::vector<index>> m_element_f_variable_indices_lo;
     std::vector<std::vector<index>> m_element_f_variable_indices_hi;
 
     SparseStructure<double, int, true> m_structure_dg;
@@ -420,24 +419,21 @@ public: // constructors
 
         Log::task_step("Initialize element boundaries...");
 
-        m_element_f_variable_indices_lo.resize(nb_elements_f);
         m_element_f_variable_indices_hi.resize(nb_elements_f);
 
         for (index i = 0; i < nb_elements_f; i++)
         {
             const auto& element_indices = m_element_f_variable_indices[i];
 
-            std::vector<index> element_lo(length(element_indices));
             std::vector<index> element_hi(length(element_indices));
 
             for (index row_i = 0; row_i < length(element_indices); row_i++) {
                 const auto row = element_indices[row_i];
+                const auto col = element_indices.back();
 
-                element_lo[row_i] = m_structure_hm.get_index(row.global, row.global);
-                element_hi[row_i] = m_structure_hm.get_index(row.global, element_indices.back().global) + 1;
+                element_hi[row_i] = m_structure_hm.get_index(row.global, col.global) + 1;
             }
 
-            m_element_f_variable_indices_lo[i] = std::move(element_lo);
             m_element_f_variable_indices_hi[i] = std::move(element_hi);
         }
 
@@ -485,7 +481,7 @@ private: // methods: computation
 
             data.df(row.global) += g(row.local);
 
-            auto lo = m_element_f_variable_indices_lo[i][row_i];
+            auto lo = m_structure_hm.get_first_index(row.global);
             const auto hi = m_element_f_variable_indices_hi[i][row_i];
 
             for (index col_i = row_i; col_i < length(variable_indices) && TOrder > 1; col_i++) {
