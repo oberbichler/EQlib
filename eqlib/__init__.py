da589f171094a2358eddadc71a5c2f542c1d8d53
Thomas Oberbichler <thomas.oberbichler@gmail.com>
2022-05-16 12:08:34 +0200

    Add `importlib_metadata` for py<3.8

--- eqlib/__init__.py
+++ eqlib/__init__.py
@@ -1,5 +1,11 @@
-import importlib.metadata as importlib_metadata
+try:
+    import importlib.metadata as importlib_metadata
+except ModuleNotFoundError:
+    import importlib_metadata
 
-__version__ = importlib_metadata.version(__name__)
+try:
+    __version__ = importlib_metadata.version(__name__)
+except:
+    __version__ = 'dev'
 
 from eqlib.eqlib_ext import Constraint, Node, Objective, Problem
