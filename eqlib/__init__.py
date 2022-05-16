0e88817889909c1da9d663e730b5eae2a4ed52e3
Thomas Oberbichler <thomas.oberbichler@gmail.com>
2022-05-16 11:27:16 +0200

    Use `importlib_metadata`

--- eqlib/__init__.py
+++ eqlib/__init__.py
@@ -1,1 +1,5 @@
-from eqlib.eqlib_ext import Constraint, Node, Objective, Problem, __version__
+import importlib.metadata as importlib_metadata
+
+__version__ = importlib_metadata.version(__name__)
+
+from eqlib.eqlib_ext import Constraint, Node, Objective, Problem
